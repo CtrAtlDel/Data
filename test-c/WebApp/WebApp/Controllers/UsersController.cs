5fcdf8051773bdf114cdee4acb3f4a36073102c4
CtrAtlDelete <kvasovski34@gmail.com>
2022-05-09 19:39:59 +0300

    Bug fix

--- test-c/WebApp/WebApp/Controllers/UsersController.cs
+++ test-c/WebApp/WebApp/Controllers/UsersController.cs
@@ -6,13 +6,25 @@ namespace WebApp.Controllers;
 [Route("/api/[controller]")]
 public class UsersController : Controller
 {
+    //Our database (imitation)
     private static List<User> _users = new List<User>(new[]
     {
         new User()
         {
-            Login = "admin", Password = "admin", Name = "admin", Gender = 1, Birthday = null, Admin = true,
+            Guid = Guid.NewGuid() ,Login = "admin", Password = "admin", Name = "admin", Gender = 1, Birthday = null, Admin = true,
             CreatedOn = DateTime.Today, CreatedBy = "admin", ModifiedOn = DateTime.Today, ModifiedBy = "admin",
             RevorkedOn = DateTime.Today, RevorkedBy = "admin"
         }
     });
+    
+    //Create
+    
+    
+    
+    //Read
+    
+    //Update
+    
+    //Delete
+    
 }
