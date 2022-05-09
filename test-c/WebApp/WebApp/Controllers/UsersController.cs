c9c771674efcc5ae5bf64f92088642605e20c88d
CtrAtlDelete <kvasovski34@gmail.com>
2022-05-09 20:01:18 +0300

    Add modification in usersController

--- test-c/WebApp/WebApp/Controllers/UsersController.cs
+++ test-c/WebApp/WebApp/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using WebApp.Model;
 
 namespace WebApp.Controllers;
 
+[ApiController]
 [Route("/api/[controller]")]
 public class UsersController : Controller
 {
@@ -11,20 +12,25 @@ public class UsersController : Controller
     {
         new User()
         {
-            Guid = Guid.NewGuid() ,Login = "admin", Password = "admin", Name = "admin", Gender = 1, Birthday = null, Admin = true,
+            Guid = Guid.NewGuid(), Login = "admin", Password = "admin", Name = "admin", Gender = 1, Birthday = null,
+            Admin = true,
             CreatedOn = DateTime.Today, CreatedBy = "admin", ModifiedOn = DateTime.Today, ModifiedBy = "admin",
             RevorkedOn = DateTime.Today, RevorkedBy = "admin"
         }
     });
     
+    //Test methods
+
+    [HttpGet]   
+    IEnumerable<User> Get() => _users;
+
     //Create
-    
-    
-    
+
+
+
     //Read
-    
+
     //Update
-    
+
     //Delete
-    
 }
